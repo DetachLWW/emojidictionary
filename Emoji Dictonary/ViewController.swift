de4f212a876bf22c5759e27a6063c1d822007e0b
Lars Huusom <larshuusom@Larss-MacBook-Pro.local>
2017-07-13 12:13:33 +0200

    Added table with text values.

--- Emoji Dictonary/ViewController.swift
+++ Emoji Dictonary/ViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var theViewTable: UITableView!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        theViewTable.dataSource = self
+        
+        theViewTable.delegate = self
+
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "HELLO THERE"
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
